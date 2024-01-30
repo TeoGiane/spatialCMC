01b1f22ee449c32b4532270271e1debc246cdeec
TeoGiane <gianeteo@hotmail.it>
2024-01-30 17:38:06 +0100

    spatial_partition --> shard_partition

--- src/shard_partition.h
+++ src/shard_partition.h
@@ -1,12 +1,9 @@
-#ifndef SPATIALCMC_SPATIAL_PARTITION_H
-#define SPATIALCMC_SPATIAL_PARTITION_H
+#ifndef SPATIALCMC_SHARD_PARTITION_H
+#define SPATIALCMC_SHARD_PARTITION_H
 
 // STL
 #include <iostream>
 #include <deque>
-
-// GEOS C API
-// #include <geos_c.h>
 
 // bayesmix
 #include <src/includes.h>
@@ -17,15 +14,9 @@
 // spatialcmc
 #include "utils.h"
 
-class SpatialPartition {
+class ShardPartition {
+
  private:
-
-	// GEOS context handler
-	// GEOSContextHandle_t ctx;
-
-	// Just for printing stuff
-	// GEOSWKTWriter* writer = nullptr;
-	// char* geom_string = nullptr;
 
 	// Class members
 	Eigen::MatrixXd data;
@@ -33,25 +24,18 @@ class SpatialPartition {
 	size_t num_cluster;
 	std::deque<unsigned int> global_clust_idx;
 	bayesmix::AlgorithmState::HierarchyHypers prior_params;
-	// GEOSGeometry* geometry = nullptr;
 	std::shared_ptr<AbstractHierarchy> hierarchy;
 
 	// Type aliases
 	using ClustState = bayesmix::AlgorithmState::ClusterState;
 
- public:
+	public:
 
 	// Constructor
-	SpatialPartition() = default;
-	
-	// Copy-constructor
-	// SpatialPartition(const SpatialPartition & sp);
-
-	// Copy-assignment
-	// SpatialPartition & operator=(const SpatialPartition & sp);
+	ShardPartition() = default;
 
 	// Destructor
-	~SpatialPartition() = default;
+	~ShardPartition() = default;
 
 	// Setters
 	void set_shard_name(const size_t & _shard);
@@ -59,7 +43,6 @@ class SpatialPartition {
 	void set_data(const Eigen::MatrixXd & _data);
 	void set_global_cluster_idx(const std::deque<unsigned int> & _global_clust_idx);
 	void set_prior_params(const bayesmix::AlgorithmState::HierarchyHypers & _prior_params);
-	// void set_geometry(GEOSGeometry* & _geometry);
 	void set_hierarchy(std::shared_ptr<AbstractHierarchy> _hierarchy);
 
 	// Getters
@@ -68,21 +51,28 @@ class SpatialPartition {
 	std::deque<unsigned int> get_global_cluster_idx() const;
 	bayesmix::AlgorithmState::HierarchyHypers get_prior_params() const;
 	size_t get_num_cluster() const;
-	// GEOSGeometry* get_geometry() const;
 
 	// Merge current spatial partition with the input spatial partition
-	void merge(const SpatialPartition & rhs);
+	void merge(const ShardPartition & rhs);
 
-	// Generate n samples from the prior or the full conditional of the hierarchy
-	// Eigen::MatrixXd SpatialPartition::sample(size_t n, bool prior);
-	Eigen::VectorXd sample(size_t n, bool prior);
-
-	// Questa cosa va migliorata perché può dipendere dalla gerarchia
-	// Eigen::VectorXd get_merging_parameter(const ClustState & state);
-	double get_merging_parameter(const ClustState & state);
+	// Returns a sample from the full conditional distribution of the hierarchy
+	std::shared_ptr<ClustState> sample_full_cond() {
+		hierarchy->sample_full_cond();
+		return hierarchy->get_state_proto();
+	}
 
 	// Print utility for debug
 	void print();
+
+	// Generate n samples of the quantity of interes either from the prior
+	// or the full conditional of the hierarchy.
+	Eigen::VectorXd sample_qoi(size_t n, bool prior);
+
+	private:
+
+	// Compute quantity of interest from sampled state of the hierarchy
+	double qoi_from_state(const ClustState & state) const ;
+
 };
 
-#endif // SPATIALCMC_SPATIAL_PARTITION_H
+#endif // SPATIALCMC_SHARD_PARTITION_H
