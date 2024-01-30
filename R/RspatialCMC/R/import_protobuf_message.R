67112cc87a2a818258a1ae272f12c41ebc4fb953
TeoGiane <gianeteo@hotmail.it>
2024-01-30 18:10:45 +0100

    Minor code fix

--- R/RspatialCMC/R/import_protobuf_message.R
+++ R/RspatialCMC/R/import_protobuf_message.R
@@ -10,15 +10,14 @@
 #' @export
 import_protobuf_messages <- function() {
 
-  # Deduce BUILD_DIR from RSPATIALCMC_EXE variable in RspatialCMC.Renviron
-  BUILD_DIR <- dirname(Sys.getenv("RSPATIALCMC_EXE"))
+  # Get SPATIALCMC_HOME variable in RspatialCMC.Renviron
   SPATIALCMC_HOME <- dirname(dirname(Sys.getenv("RSPATIALCMC_HOME")))
 
   # Deduce protocol buffer proto paths
-  bayesmix_protoPath <- sprintf("%s/_deps/bayesmix-src/src/proto", BUILD_DIR)
+  bayesmix_protoPath <- sprintf("%s/lib/bayesmix-src/src/proto", SPATIALCMC_HOME)
   spatialcmc_protoPath <- sprintf("%s/src/proto", SPATIALCMC_HOME)
 
-  # Import protocol buffer message descripts via RProtoBuf
+  # Import protocol buffer message descriptors via RProtoBuf
   RProtoBuf::readProtoFiles2(protoPath = bayesmix_protoPath)
   RProtoBuf::readProtoFiles2(protoPath = spatialcmc_protoPath)
 }
