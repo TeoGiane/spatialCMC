78c7e884af88fedee5f0e073b4441a6ef3e665c0
TeoGiane <gianeteo@hotmail.it>
2024-01-30 17:41:47 +0100

    Improved error handling

--- R/RspatialCMC/R/build_spatialCMC.R
+++ R/RspatialCMC/R/build_spatialCMC.R
@@ -15,51 +15,87 @@ build_spatialCMC <- function(nproc = ceiling(parallel::detectCores()/2), build_d
   if(!is.numeric(nproc)) { stop("nproc must be a number") }
   if(!is.character(build_dirname)) { stop("build_dirname must be a string") }
 
+  # Get .Renviron file from package
+  renviron = system.file("RspatialCMC.Renviron", package = "RspatialCMC")
+
   # Set spatialcmc_home folder from RSPATIALCMC_HOME
+  readRenviron(renviron)
   home_dir = Sys.getenv("RSPATIALCMC_HOME")
+  if(home_dir == ""){
+    stop("RSPATIALCMC_HOME environment variable is not set")
+  }
   spatialcmc_home = dirname(dirname(home_dir))
 
-  # Build spatialCMC library
+  # Create build/ subdirectory
   build_dir = sprintf("%s/%s", spatialcmc_home, build_dirname)
-  cat("*** Configuring spatialCMC ***\n")
+  dir.create(build_dir, showWarnings = F)
 
-  CONFIGURE = sprintf("mkdir -p %s && cd %s && cmake ..", build_dir, build_dir)
-  tryCatch({
-    system(CONFIGURE, ignore.stdout = TRUE, ignore.stderr = TRUE)
-  },
-  error = function(e) {
-    ERR_MSG <- "Something went wrong during configure. Failed with error:\n%s\n"
-    stop(sprintf(ERR_MSG, as.character(e)))
-  })
+  # Configure spatialCMC
+  cat("*** Configuring spatialCMC ***\n")
+  CONFIGURE = 'cmake .. -G "Unix Makefiles"'
+  errlog <- withr::with_dir(build_dir, system(CONFIGURE, ignore.stderr = TRUE))
+  if(errlog != 0L){
+    errmsg <- "Something went wrong during configure: command '%s' exit with status %d"
+    stop(sprintf(errmsg, CONFIGURE, errlog))
+  }
+  cat("\n")
+
+  # CONFIGURE = sprintf("mkdir -p %s && cd %s && cmake ..", build_dir, build_dir)
+  # tryCatch({
+  #   system(CONFIGURE, ignore.stderr = TRUE)
+  # },
+  # error = function(e) {
+  #   ERR_MSG <- "Something went wrong during configure. Failed with error:\n%s\n"
+  #   stop(sprintf(ERR_MSG, as.character(e)))
+  # })
 
   # errlog = system(CONFIGURE, ignore.stdout = TRUE, ignore.stderr = TRUE)
   # if(errlog != 0) { stop(sprintf("Something went wrong during configure: exit with status %d", errlog)) }
 
   # Make run_cmc executable
   cat("*** Building run_cmc executable ***\n")
-  BUILD = sprintf("cd %s && make run_cmc -j%d", build_dir, nproc)
-  errlog = system(BUILD)
-  if(errlog != 0) { stop(sprintf("Something went wrong during build: exit with status %d", errlog)) }
+  BUILD = sprintf("make run_cmc -j%d", nproc)
+  errlog <- withr::with_dir(build_dir, system(BUILD))
+  if (errlog != 0L) {
+    errmsg <- "Something went wrong during build: command '%s' exit with status %d"
+    stop(sprintf(errmsg, BUILD, errlog))
+  }
+  cat("\n")
+
+  # errlog = system(BUILD)
+  # if(errlog != 0) { stop(sprintf("Something went wrong during build: exit with status %d", errlog)) }
 
   # Make run_mcmc executable
   cat("*** Building run_mcmc executable ***\n")
-  BUILD = sprintf("cd %s && make run_mcmc -j%d", build_dir, nproc)
-  errlog = system(BUILD)
-  if(errlog != 0) { stop(sprintf("Something went wrong during build: exit with status %d", errlog)) }
+  BUILD = sprintf("make run_mcmc -j%d", nproc)
+  errlog <- withr::with_dir(build_dir, system(BUILD))
+  if (errlog != 0L) {
+    errmsg <- "Something went wrong during build: command '%s' exit with status %d"
+    stop(sprintf(errmsg, BUILD, errlog))
+  }
+  cat("\n")
+
+  # errlog = system(BUILD)
+  # if(errlog != 0) { stop(sprintf("Something went wrong during build: exit with status %d", errlog)) }
 
   # Create .renviron file
-  renviron = system.file("RspatialCMC.Renviron", package = "RspatialCMC")
+  # renviron = system.file("RspatialCMC.Renviron", package = "RspatialCMC")
 
-  # Set SPATIALCMC_EXE environment variable
+  # Set RSPATIALCMC_EXE environment variable
   cat("*** Setting RSPATIALCMC_EXE environment variable ***\n")
   write(x = sprintf("RSPATIALCMC_EXE=%s/run_cmc", build_dir), file = renviron, append = TRUE)
+
   # Set MCMC_EXE environment variable
   cat("*** Setting MCMC_EXE environment variable ***\n")
   write(x = sprintf("MCMC_EXE=%s/run_mcmc", build_dir), file = renviron, append = TRUE)
 
-  # Parse .renviron file
-  readRenviron(renviron)
+  # Set TBB_PATH environment variable (for Windows)
+  cat("*** Setting TBB_PATH environment variable ***\n")
+  tbb_path = sprintf('%s/lib/bayesmix-src/lib/_deps/math-src/lib/tbb/', spatialcmc_home)
+  write(x = sprintf('TBB_PATH=%s', tbb_path), file = renviron, append = TRUE)
+  cat("\n")
 
-  cat("spatialCMC successfully installed and environment variables set\n")
-  # return(TRUE)
+  # Parse .Renviron file to get environment variables
+  readRenviron(renviron)
+  cat("Successfully installed spatialCMC\n")
 }
