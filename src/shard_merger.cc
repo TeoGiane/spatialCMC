db419a77042562a129c83c63f34ac156c4bb07cc
TeoGiane <gianeteo@hotmail.it>
2024-04-24 11:44:26 +0200

    Small change in QOI implementation

--- src/shard_merger.cc
+++ src/shard_merger.cc
@@ -59,8 +59,7 @@ bayesmix::AlgorithmState ShardMerger::merge(size_t iter) {
 		auto merge_condition = [this, &lhs/*, &ctx*/] (ShardPartition & rhs) {
 			bool merge = (lhs.get_shard_name() != rhs.get_shard_name());
 			if(merge) { merge *= intersects(lhs, rhs); }
-			// if(merge) { merge *= GEOSIntersects_r(ctx, lhs.get_geometry(), rhs.get_geometry()); }
-			if(merge) { merge *= (2*compute_bayes_factor(lhs, rhs) > 5.0); }
+			if(merge) { merge *= (2*compute_logBF(lhs, rhs) > 2.0); }
 			if(merge) { lhs.merge(rhs); }
 			return merge;
 		};
@@ -89,15 +88,12 @@ bayesmix::AlgorithmState ShardMerger::merge(size_t iter) {
 		// clust_state.set_cardinality(elem.get_card());
 		merged_state.add_cluster_states()->CopyFrom(*elem.sample_full_cond());
 		// Computing global cluster allocs
-		global_cluster_allocs(elem.get_global_cluster_idx()).array() = k;//elem.get_num_cluster();
+		global_cluster_allocs(elem.get_global_cluster_idx()).array() = k;
 		k++;
 	}
 
 	// Set new cluster allocs in merged state message
 	*merged_state.mutable_cluster_allocs() = { global_cluster_allocs.begin(), global_cluster_allocs.end() };
-
-	// Memory management
-	// GEOS_finish_r(ctx);
 
 	// Check - OK
 	// std::cout << merged_state.DebugString() << std::endl;
@@ -107,11 +103,6 @@ bayesmix::AlgorithmState ShardMerger::merge(size_t iter) {
 }
 
 std::deque<ShardPartition> ShardMerger::generate_local_clusters(const size_t & iter) {
-
-	// Initialize GEOS
-	// GEOSContextHandle_t ctx = GEOS_init_r();
-	// GEOSContext_setNoticeHandler_r(ctx, spatialcmc::geos_msg_handler);
-	// GEOSContext_setErrorHandler_r(ctx, spatialcmc::geos_msg_handler);
 	
 	// Shuffle shards parsing order
 	// std::vector<unsigned int> parse_order(num_shards);
@@ -140,25 +131,16 @@ std::deque<ShardPartition> ShardMerger::generate_local_clusters(const size_t & i
 		auto adj_matrix_in_shard = shards[it].get_adjacency_matrix();
 
 		// Get local cluster indexes and partition in each cluster
-		// std::vector<std::vector<GEOSGeometry*>> geom_in_clust(curr_state.cluster_states_size());
-		// std::vector<GEOSGeometry*> geom_in_clust(curr_state.cluster_states_size(), GEOSGeom_createEmptyCollection_r(ctx, GEOS_GEOMETRYCOLLECTION));
 		std::vector<std::deque<unsigned int>> local_clust_idx(curr_state.cluster_states_size());
 		std::vector<std::deque<unsigned int>> global_clust_idx(curr_state.cluster_states_size());
 		for (size_t j = 0; j < curr_state.cluster_allocs_size(); j++) {
 			unsigned int c = curr_state.cluster_allocs(j);
-			// GEOSGeometry* old_geom = geom_in_clust[c];
-			// geom_in_clust[c].push_back(GEOSGeom_clone_r(ctx, GEOSGetGeometryN_r(ctx, geom_in_shard, j)));
-			// geom_in_clust[c] = GEOSUnion_r(ctx, old_geom, GEOSGetGeometryN_r(ctx, geom_in_shard, j));
-			// GEOSGeom_destroy_r(ctx, old_geom);
 			local_clust_idx[c].push_back(j);
 			global_clust_idx[c].push_back(data_idx_in_shard[it][j]);
 		}
 
 		// Building ShardPartition list
 		for (size_t k = 0; k < curr_state.cluster_states_size(); k++) {
-			// Create geometry collection from vector of geometries
-			// GEOSGeometry* geom = GEOSUnaryUnion_r(ctx, GEOSGeom_createCollection_r(ctx, GEOSGeomTypes::GEOS_GEOMETRYCOLLECTION,
-			// 																		 																	 geom_in_clust[k].data(), geom_in_clust[k].size()));
 			// Construct and fill spatial partition
 			ShardPartition sp_to_add;
 			sp_to_add.set_shard_name(it);
@@ -171,50 +153,38 @@ std::deque<ShardPartition> ShardMerger::generate_local_clusters(const size_t & i
 			sp_to_add.set_hierarchy(shards[it].get_hierarchy());
 			// Add spatial partition
 			local_clusters.push_back(sp_to_add);
-			// Memory management
-			// GEOSGeom_destroy_r(ctx, geom_in_clust[k]);
 		}
-
 		// Update global cluster counter
 		clust_counter += curr_state.cluster_states_size();
-
-		// Memory management
-		// GEOSGeom_destroy_r(ctx, geom_in_shard);
 	}
-
-	// Memory management
-	// GEOS_finish_r(ctx);
 
 	// Return
 	return local_clusters;
 }
 
-double ShardMerger::compute_bayes_factor(ShardPartition & lhs, ShardPartition & rhs) {
-	// Monte Carlo samples from prior and posterior
+double ShardMerger::compute_logBF(ShardPartition & lhs, ShardPartition & rhs) {
+	// Number of Monte Carlo samples
 	size_t n_sim = 50;
 	// Eigen::VectorXd l2_prior(n_sim), l2_post(n_sim); bool prior = true;
-	// #pragma omp single
-	// {
-	// for (size_t k = 0; k < n_sim; k++) {
-	// 	// #pragma omp critical
-	// 	//std::cout << "k: " << k << std::endl;
-	// 	l2_prior(k) = sample_l2_distance(lhs, rhs, prior);
-	// 	// #pragma omp critical
-	// 	//std::cout << "l2_prior: " << l2_prior(k) << std::endl;
-	// 	l2_post(k) = sample_l2_distance(lhs, rhs, !prior);
-	// 	// #pragma omp critical
-	// 	//std::cout << "l2_post: " << l2_post(k) << std::endl;
-	// }
-	// }
+  Eigen::VectorXd qoi_prior(n_sim), qoi_post(n_sim); bool prior = true;
+  // Compute Monte Carlo samples from prior and posterior
+	for (size_t k = 0; k < n_sim; k++) {
+    // Using L2 distance
+		// l2_prior(k) = sample_l2_distance(lhs, rhs, prior);
+    // l2_post(k) = sample_l2_distance(lhs, rhs, !prior);
+    // Using L1 distance of the parameter's vector
+  	qoi_prior(k) = sample_qoi(lhs, rhs, prior);
+    qoi_post(k) = sample_qoi(lhs, rhs, !prior);
+	}
 	
-	auto lhs_prior = lhs.sample_qoi(n_sim, true);
-	auto rhs_prior = rhs.sample_qoi(n_sim, true);
-	auto lhs_post = lhs.sample_qoi(n_sim, false);
-	auto rhs_post = rhs.sample_qoi(n_sim, false);
+	// auto lhs_prior = lhs.sample_qoi(n_sim, true);
+	// auto rhs_prior = rhs.sample_qoi(n_sim, true);
+	// auto lhs_post = lhs.sample_qoi(n_sim, false);
+	// auto rhs_post = rhs.sample_qoi(n_sim, false);
 
 	// Compute quantity of interest
-	Eigen::VectorXd qoi_post = (lhs_post - rhs_post).array().abs(); // / (lhs_post.array().abs() + rhs_post.array().abs());
-	Eigen::VectorXd qoi_prior = (lhs_prior - rhs_prior).array().abs(); // / (lhs_prior.array().abs() + rhs_prior.array().abs());
+	// Eigen::VectorXd qoi_post = (lhs_post - rhs_post).array().abs(); // / (lhs_post.array().abs() + rhs_post.array().abs());
+	// Eigen::VectorXd qoi_prior = (lhs_prior - rhs_prior).array().abs(); // / (lhs_prior.array().abs() + rhs_prior.array().abs());
 
 	// Compute epsilon dynamically
 	double epsilon = stan::math::quantile(qoi_prior, 0.5);
--- src/shard_merger.h
+++ src/shard_merger.h
@@ -53,7 +53,7 @@ class ShardMerger {
  private:
 
 	// Compute bayes factor for two spatial partitions merging candidates
-	double compute_bayes_factor(ShardPartition & lhs, ShardPartition & rhs);
+	double compute_logBF(ShardPartition & lhs, ShardPartition & rhs);
 
 	// Generate a Mone Carlo sample for the L^2 distance between density functions
 	double sample_l2_distance(ShardPartition & lhs, ShardPartition & rhs, bool prior) {
@@ -65,73 +65,80 @@ class ShardMerger {
 
 	// Compute L^2 distance between two densities in closed form, using the associated parameters
 	double compute_l2_distance(bayesmix::AlgorithmState::ClusterState & lhs, bayesmix::AlgorithmState::ClusterState & rhs) {
-		// std::cout << "ShardMerger::compute_l2_distance()" << std::endl;
+		// Create buffer
 		double out = 0;
 		if (lhs.has_uni_ls_state() && rhs.has_uni_ls_state()) {
+			// Gaussian case
 			double lmean = lhs.uni_ls_state().mean(), lvar = lhs.uni_ls_state().var();
-			// std::cout << "lmean: " << lmean << ", lvar: " << lvar << std::endl;
 			double rmean = rhs.uni_ls_state().mean(), rvar = rhs.uni_ls_state().var();
-			// std::cout << "rmean: " << rmean << ", rvar: " << rvar << std::endl;
-			// #pragma omp critical
-			// std::cout << "Thread " << omp_get_thread_num() << ": lmean: " << lmean << ", lvar: " << lvar << ", rmean: " << rmean << ", lrvar: " << rvar << std::endl;
-
-			// std::cout << "out: " << out << std::endl;
+			// Compute L2 distance squared
 			out = std::exp(-(stan::math::LOG_TWO + stan::math::LOG_SQRT_PI + 0.5*std::log(lvar)));
-			// std::cout << "out: " << out << std::endl;
 			out += std::exp(-(stan::math::LOG_TWO + stan::math::LOG_SQRT_PI + 0.5*std::log(rvar)));
-			// std::cout << "out: " << out << std::endl;
 			out -= 2 * std::exp(stan::math::normal_lpdf(0, lmean - rmean, std::sqrt(lvar + rvar)));
-			// std::cout << "out: " << out << std::endl;
-			// std::cout << "end" << std::endl << std::endl;
-			// #pragma omp critical
-			// std::cout << "ShardMerger::compute_l2_distance(), result: " << out << std::endl;
-			
-			// Avoid negative zeros (apparently)
-			// if (out < 0) {
-			// 	out = std::abs(out);
-			// 	// #pragma omp critical
-			// 	// std::cout << "A NEGATIVE VALUE HAS BEEN PRODUCED!" << std::endl;
-			// }
+			// Return
 			return out;
-			// return std::sqrt(out);
 		} else if (lhs.has_custom_state() && rhs.has_custom_state()) {
-			// std::cout << "ShardMerger::compute_l2distance()" << std::endl;
+			// Poisson case
 			spatialcmc::PoissonState unp_state; 
 			lhs.custom_state().UnpackTo(&unp_state); double lrate = unp_state.rate();
-			// std::cout << "lrate: " << lrate << std::endl;
 			rhs.custom_state().UnpackTo(&unp_state); double rrate = unp_state.rate();
-			// std::cout << "rrate: " << rrate << std::endl;
-
+			// Compute L2 distance squared
 			out = std::exp(-2*lrate + stan::math::log_modified_bessel_first_kind(0, 2*lrate));
-			// std::cout << "out[1]: " << out << std::endl;
 			out += std::exp(-2*rrate + stan::math::log_modified_bessel_first_kind(0, 2*rrate));
-			// std::cout << "out[2]: " << out << std::endl;
 			out -= 2*std::exp(-lrate -rrate + stan::math::log_modified_bessel_first_kind(0, 2*std::sqrt(lrate*rrate)));
-			// std::cout << "out[3]: " << out << std::endl;
-
+			// Return
 			return out;
-
 		} else {
+			// No available cluster state
 			throw std::runtime_error("compute_l2_distance() not implemented for this cluster state.");
 		}
 	}
 
+	// Generate a Mone Carlo sample for the L^2 distance between density functions
+	double sample_qoi(ShardPartition & lhs, ShardPartition & rhs, bool prior) {
+		bayesmix::AlgorithmState::ClusterState state_lhs, state_rhs;
+		state_lhs = prior ? *(lhs.sample_prior()) : *(lhs.sample_full_cond());
+		state_rhs = prior ? *(rhs.sample_prior()) : *(rhs.sample_full_cond());
+		return compute_qoi(state_lhs, state_rhs);
+	}
+
+	// Compute L^2 distance between two densities in closed form, using the associated parameters
+	double compute_qoi(bayesmix::AlgorithmState::ClusterState & lhs, bayesmix::AlgorithmState::ClusterState & rhs) {
+		// Create buffer
+		double out = 0;
+		if (lhs.has_uni_ls_state() && rhs.has_uni_ls_state()) {
+			// Gaussian case
+			double lmean = lhs.uni_ls_state().mean(), lvar = lhs.uni_ls_state().var();
+			double rmean = rhs.uni_ls_state().mean(), rvar = rhs.uni_ls_state().var();
+			// Compute sum of absolute distance between each element of parameters' vector
+			out = std::abs(lmean - rmean) + std::abs(lvar - rvar);
+			// Return
+			return out;
+		} else if (lhs.has_custom_state() && rhs.has_custom_state()) {
+			// Poisson case
+			spatialcmc::PoissonState unp_state; 
+			lhs.custom_state().UnpackTo(&unp_state); double lrate = unp_state.rate();
+			rhs.custom_state().UnpackTo(&unp_state); double rrate = unp_state.rate();
+			// Compute sum of absolute distance between each element of parameters' vector
+			out = std::abs(lrate - rrate);
+			// Return
+			return out;
+		} else {
+			// No available cluster state
+			throw std::runtime_error("compute_qoi() not implemented for this cluster state.");
+		}
+	}
+
 	bool intersects(ShardPartition & lhs, ShardPartition & rhs) {
-		// std::cout << "ShardMerger::intersects()" << std::endl;
 		// Concatenating vertices
 		auto idx = lhs.get_global_cluster_idx();
 		int n_row = idx.size();
 		auto rhs_idx = rhs.get_global_cluster_idx();
 		int n_col = rhs_idx.size();
-
 		// Stack index vectors
 		idx.insert(idx.end(), rhs_idx.begin(), rhs_idx.end());
-
 		// Check inersection
 		bool condition = global_adj_matrix(idx, idx).topRightCorner(n_row, n_col).sum() > 0;
-		// std::cout << "Mat: (" <<  mat.rows() << "," << mat.cols() << ")" << std::endl;
-		// std::cout << mat << std::endl;
-
 		// Return
 		return condition;
 	};
