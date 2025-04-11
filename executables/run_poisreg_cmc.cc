7b3f05debfc60fb2d6d6c4f5116ff77060aad337
TeoGiane <gianeteo@hotmail.it>
2025-04-11 16:02:58 +0100

    Poisson Regression CMC works

--- executables/run_poisreg_cmc.cc
+++ executables/run_poisreg_cmc.cc
@@ -5,6 +5,8 @@
 
 // OpenMP
 #include <omp.h>
+#include <stan/math.hpp>
+#include <Eigen/Dense>
 
 // argparse
 #include "lib/argparse/argparse.h"
@@ -14,9 +16,8 @@
 
 // spatialcmc
 #include "algorithm/poisson_regression_sampler.h"
-#include "cmc/shard.h"
-#include "cmc/shard_merger.h"
-#include "cmc/shard_partition.h"
+#include "cmc/local_cluster_merger.h"
+#include "cmc/regression_coefficients_merger.h"
 #include "cmc/spatialcmc_utils.h"
 #include "hierarchies/poisson_regression_hierarchy.h"
 #include "mixing/spp_mixing.h"
@@ -44,7 +45,27 @@ void check_args(const argparse::ArgumentParser &args) {
     std::exit(1);
   }
   return;
-}
+};
+
+void run_mcmc(std::shared_ptr<PoissonRegressionSampler> algo,
+              std::vector<bayesmix::AlgorithmState> & partition_chain,
+              std::vector<bayesmix::Vector> & reg_coeffs_chain) {
+  // Run the MCMC algorithm
+  BaseCollector * coll = new MemoryCollector();
+  algo->run(coll);
+  // Create chain buffers
+  partition_chain.resize(coll->get_size());
+  reg_coeffs_chain.resize(coll->get_size());
+  // Fill the chains containers
+  spatialcmc::PoisRegAlgorithmState curr_state;
+  for (int i = 0; i < coll->get_size(); i++) {
+    coll->get_next_state(&curr_state);
+    partition_chain[i].CopyFrom(curr_state.partition());
+    reg_coeffs_chain[i].CopyFrom(curr_state.regression_coefficients());
+  }
+  // Deallocate collector
+  delete coll;
+};
 
 /* Main Function - SpatialCMC Sampler */
 
@@ -102,7 +123,7 @@ int main(int argc, char const *argv[]) {
 
   // Startup message
   std::cout << std::endl;
-  std::cout << "Running run_cmc.cc" << std::endl;
+  std::cout << "Running run_poisson_regression_cmc.cc" << std::endl;
 
   // Read data files
   auto data = bayesmix::read_eigen_matrix(args.get<std::string>("--data-file"));
@@ -110,96 +131,98 @@ int main(int argc, char const *argv[]) {
   auto adj_matrix = bayesmix::read_eigen_matrix(args.get<std::string>("--adj-matrix-file"));
   auto shard_allocation = spatialcmc::read_shard_allocation_file(args.get<std::string>("--shard-assignment-file"));
 
-  // Create hierarchy object (NON SERVE)
-  // std::shared_ptr<AbstractHierarchy> hierarchy;
-  // if(args.get<std::string>("--hier-type") == "PoissonGamma") {
-  //   hierarchy = std::make_shared<PoissonGammaHierarchy>();
-  // } else if (args.get<std::string>("--hier-type") == "Empty") {
-  //   hierarchy = std::make_shared<EmptyHierarchy>();
-  // } else {
-  //   auto & factory_hier = HierarchyFactory::Instance();
-  //   hierarchy = factory_hier.create_object(args.get<std::string>("--hier-type"));
-  // }
-  
-  // Create mixing object (NON SERVE)
-  // std::shared_ptr<AbstractMixing> mixing;
-  // if(args.get<std::string>("--mix-type") == "sPP") {
-  //   mixing = std::make_shared<sPPMixing>();
-  // } else {
-  //   auto & factory_mixing = MixingFactory::Instance();
-  //   mixing = factory_mixing.create_object(args.get<std::string>("--mix-type"));
-  // }
-
-  // Split data in shards (OK)
+  // Split data in shards
   unsigned int num_shards = std::set<int>(shard_allocation.cbegin(), shard_allocation.cend()).size();
   std::vector<std::deque<int>> global_numbering(num_shards);
   for (size_t i = 0; i < data.size(); i++) {
     global_numbering[shard_allocation[i]].push_back(i);
   }
 
-  // Generating shards in parallel (SERVE POISREG_SHARD)
-  // std::vector<Eigen::MatrixXd> data_in_shards(num_shards); (NON SERVE)
-  // std::vector<PoissonRegShard> shards(num_shards);
-  // #pragma omp parallel for num_threads(num_threads)
-	// for (size_t i = 0; i < num_shards; i++) {
-  //   shards[i].set_data(data(global_numbering[i], 0));
-  //   if(mixing->is_dependent()){
-  //     shards[i].set_adjacency_matrix(adj_matrix(global_numbering[i], global_numbering[i]));
-  //   }
-  //   shards[i].set_sampler_parameters(args.get<std::string>("--algo-params-file"));
-  //   shards[i].set_hierarchy(hierarchy->clone(), args.get<std::string>("--hier-prior-file"));
-  //   shards[i].set_mixing(mixing->clone(), args.get<std::string>("--mix-prior-file"));
-  //   if(i != 0) 
-  //     shards[i].set_verbose(false);
-  // }
+  // Read algorithm parameters
+  bayesmix::AlgorithmParams algo_proto;
+  bayesmix::read_proto_from_file(args.get<std::string>("--algo-params-file"), &algo_proto);
 
-  // FINO A QUI!!!
+  // Generating shards
+  std::vector<std::shared_ptr<PoissonRegressionSampler>> shards(num_shards);
+  std::cout << "Generating shards ... ";
+	for (size_t i = 0; i < num_shards; i++) {
+    shards[i] = std::make_shared<PoissonRegressionSampler>();
+    shards[i]->set_data(data(global_numbering[i], 0));
+    shards[i]->set_hier_covariates(cov_matrix(global_numbering[i], Eigen::all));
+    shards[i]->set_adjacency_matrix(adj_matrix(global_numbering[i], global_numbering[i]));
+    shards[i]->set_hierarchy_prior(args.get<std::string>("--hier-prior-file"));
+    shards[i]->set_mixing_prior(args.get<std::string>("--mix-prior-file"));
+    shards[i]->read_params_from_proto(algo_proto);
+    if(i != 0) 
+      shards[i]->set_verbose(false);
+  }
+  std::cout << "Done" << std::endl;
 
   // Check - OK
   // std::cout << "N° of shards: " << shards.size() << std::endl;
   // for (auto && elem : shards) { elem.print(); std::cout << std::endl; }
 
-  // Run MCMC samplers in each shard - in parallel --> ConsensusMCSampler::parallel_sample();
-  // std::vector<spatialcmc::MCMCChain> chains(num_shards);
-  // #pragma omp parallel for num_threads(num_threads)
-  // for (size_t i = 0; i < num_shards; i++) {
-  //   chains[i] = shards[i].sample();
+  // Run MCMC samplers in each shard - in parallel
+  std::vector<std::vector<bayesmix::AlgorithmState>> sharded_partitions(num_shards);
+  std::vector<std::vector<bayesmix::Vector>> sharded_regression_coefficients(num_shards);
+  #pragma omp parallel for num_threads(num_threads)
+  for (size_t i = 0; i < num_shards; i++) {
+    run_mcmc(shards[i], sharded_partitions[i], sharded_regression_coefficients[i]);
+  }
+
+  // // CHECK
+  // for (size_t i = 0; i < num_shards; i++){
+  //   std::cout << "Shard: " << i << std::endl;
+  //   std::cout << "Size: " << sharded_partitions[i].size() << std::endl;
+  //   std::cout << "First Partition message:\n" << sharded_partitions[i][0].DebugString() << std::endl;
+  //   std::cout << "First Reg. Coeffs. message:\n" << sharded_regression_coefficients[i][0].DebugString() << std::endl;
   // }
 
-  // // Prepare buffers for merging MCMC chains in shards
-  // ShardMerger shard_merger(shards, chains, global_numbering, adj_matrix);
-  // std::vector<bayesmix::AlgorithmState> state_vect(shard_merger.get_num_iter());
-  // progresscpp::ProgressBar* bar = new progresscpp::ProgressBar(shard_merger.get_num_iter(), 60);
+  // Set-up partition merger
+  LocalClusterMerger<PoissonRegHierarchy> partition_merger(sharded_partitions);
+  partition_merger.set_data(&data);
+  partition_merger.set_cov_matrix(cov_matrix);
+  partition_merger.set_adj_matrix(&adj_matrix);
+  partition_merger.set_global_numbering(global_numbering);
+  partition_merger.set_seed(algo_proto.rng_seed());
+  partition_merger.set_hierarchy_prior(args.get<std::string>("--hier-prior-file"));
 
-  // // Merging shards in parallel
-  // std::cout << "Merging MCMC chains..." << std::endl;
-  // // #pragma omp parallel for num_threads(num_threads)
-  // for (size_t i = 0; i < state_vect.size(); i++) {
-  //   // std::cout << "ITERATION: " << i << std::endl;
-  //   state_vect[i].CopyFrom(shard_merger.merge(i));
-	// 	++(*bar);
-  //   // #pragma omp critical
-	// 	bar->display();
-  // }
-	// bar->done();
-	// std::cout << "Done" << std::endl;
+  // Set-up regression coefficients merger
+  RegressionCoefficientsMerger reg_coeffs_merger(sharded_regression_coefficients);
 
-  // // Serialization of the final chain
-  // spatialcmc::MCMCChain merged_chain;
-  // * merged_chain.mutable_state() = { state_vect.begin(), state_vect.end() };
-  // std::cout << "Successfully serialized final MCMC chain" << std::endl;
+  // Merging shards in parallel
+  std::cout << "Merging MCMC chains..." << std::endl;
+  std::vector<spatialcmc::PoisRegAlgorithmState> merged_states(partition_merger.get_num_iter());
+  progresscpp::ProgressBar* bar = new progresscpp::ProgressBar(partition_merger.get_num_iter(), 60);
+  #pragma omp parallel for num_threads(num_threads)
+  for (size_t i = 0; i < merged_states.size(); i++) {
+    auto merged_reg_coeffs_proto = reg_coeffs_merger.merge(i);
+    merged_states[i].mutable_regression_coefficients()->CopyFrom(merged_reg_coeffs_proto);
+    partition_merger.set_reg_coeffs(i, bayesmix::to_eigen(merged_reg_coeffs_proto));
+    merged_states[i].mutable_partition()->CopyFrom(partition_merger.merge(i));
+		++(*bar);
+    #pragma omp critical
+		bar->display();
+  }
+	bar->done();
+	std::cout << "Done" << std::endl;
 
-  // // Write to file in serialized format
-  // if (args["--chain-file"] != EMPTYSTR) {
-  //   std::ofstream outfile(args.get<std::string>("--chain-file"));
-  //   outfile << merged_chain.SerializeAsString();
-  //   std::cout << "Successfully wrote serialized MCMC chain to "
-  //             << args.get<std::string>("--chain-file") << std::endl;
-  // }
+  // Serialization of the final chain
+  spatialcmc::PoisRegMCMC merged_chain;
+  * merged_chain.mutable_state() = { merged_states.begin(), merged_states.end() };
+  std::cout << "Successfully serialized final MCMC chain" << std::endl;
 
-  // // Final message
-  // std::cout << "End of run_cmc.cc" << std::endl;
-  // std::cout << std::endl;
+  // Write to file in serialized format
+  if (args["--chain-file"] != EMPTYSTR) {
+    std::ofstream outfile(args.get<std::string>("--chain-file"));
+    outfile << merged_chain.SerializeAsString();
+    std::cout << "Successfully wrote serialized MCMC chain to "
+              << args.get<std::string>("--chain-file") << std::endl;
+  }
+
+  // Final message
+  std::cout << "End of run_poisson_regression_cmc.cc" << std::endl;
+  std::cout << std::endl;
 
   // Memory management
   google::protobuf::ShutdownProtobufLibrary();
@@ -207,4 +230,4 @@ int main(int argc, char const *argv[]) {
 
   // Terminate program with no error
   return 0;
-}
+};
