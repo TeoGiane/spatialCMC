22bcf3df20a66d6367cb5db4804c19068de117f6
Matteo Gianella <gianeteo@hotmail.it>
2025-08-05 16:58:00 +0200

    Simulation script with no covariates

---
+++ R/RealData/src/run_simulation.R
@@ -0,0 +1,50 @@
+# Required packages
+library("RspatialCMC")
+library("sf")
+
+# Load Dataset (remove the NAs)
+full_data <- read_sf("input/full_dataset.shp")
+full_data <- full_data[which(full_data$ET != 0),]
+
+# Choose if MCMC or CMC
+use_cmc <- TRUE
+
+# Set hierarchy parameters
+hier_prior =
+  "
+  fixed_values {
+    shape: 3
+    rate: 0.5
+  }
+  "
+
+# Set mixing parameters
+mix_prior =
+  "
+  fixed_value {
+    totalmass: 1.0
+    lambda: 0.35
+  }
+  "
+
+# Set algorithm parameters
+algo_params =
+  "
+  algo_id: 'Neal2'
+  rng_seed: 10092022
+  iterations: 5000
+  burnin: 1000
+  init_num_clusters: 5
+  "
+
+# Run SpatialCMC sampler (either MCMC or CMC)
+if(use_cmc) {
+  fit <- run_cmc(data = as.numeric(full_data$T_20), geometry = st_geometry(full_data), shard_allocation = full_data$COD_REG-1,
+                 algo_params = algo_params, "PoissonGamma", hier_prior,"sPP", mix_prior, covariates = as.matrix(full_data$ET))
+} else {
+  fit <- run_mcmc(data = as.numeric(full_data$T_20), geometry = st_geometry(full_data),
+                  algo_params = algo_params, "PoissonGamma", hier_prior,"sPP", mix_prior, covariates = as.matrix(full_data$ET))
+}
+
+# Save chain to output
+save(fit, file = "chain.dat")
