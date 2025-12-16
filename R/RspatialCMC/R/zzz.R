f462aca6726721fd2cf6c500319fd55f85fe2fae
TeoGiane <gianeteo@hotmail.it>
2025-12-16 17:25:16 +0100

    Enhance environment variable management in .onAttach and .onDetach

--- R/RspatialCMC/R/zzz.R
+++ R/RspatialCMC/R/zzz.R
@@ -1,12 +1,16 @@
 # Parse internal renviron file to set variables
 .onAttach <- function(...) {
   readRenviron(system.file("RspatialCMC.Renviron", package = "RspatialCMC"))
+  import_protobuf_messages()
 }
 
 # Unset variables on detaching
 .onDetach <- function(...) {
+  RProtoBuf::resetDescriptorPool()
   Sys.unsetenv("RSPATIALCMC_HOME")
-  Sys.unsetenv("RSPATIALCMC_EXE")
+  Sys.unsetenv("CMC_EXE")
   Sys.unsetenv("MCMC_EXE")
+  Sys.unsetenv("POISREG_CMC_EXE")
+  Sys.unsetenv("POISREG_MCMC_EXE")
   Sys.unsetenv("TBB_PATH")
 }
