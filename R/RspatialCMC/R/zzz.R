e4087393888e7bc796d30ff29c5b7359dc5a5268
TeoGiane <gianeteo@hotmail.it>
2025-12-16 18:21:11 +0100

    Hotfix

--- R/RspatialCMC/R/zzz.R
+++ R/RspatialCMC/R/zzz.R
@@ -1,12 +1,10 @@
 # Parse internal renviron file to set variables
 .onAttach <- function(...) {
   readRenviron(system.file("RspatialCMC.Renviron", package = "RspatialCMC"))
-  import_protobuf_messages()
 }
 
 # Unset variables on detaching
 .onDetach <- function(...) {
-  RProtoBuf::resetDescriptorPool()
   Sys.unsetenv("RSPATIALCMC_HOME")
   Sys.unsetenv("CMC_EXE")
   Sys.unsetenv("MCMC_EXE")
